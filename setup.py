9074e12c7923bdc48debc5b96f5507d29c2a0c5b
adamt <adamtarasov@gmail.com>
2020-08-30 11:04:07 +0300

    setup.py update

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 setup(
     name='p2',
     version='0.1',
-    description='Python Distribution Utilities',
+    description='Python Package 2',
     author='Adam Ta',
-    author_email='notrelevant@gmail.net',
-    url='https://github.com/sababa11/p1.git',
-    install_requires=['boto==1.14.51', 'p1==0.1'],
+    author_email='notrelevant@mail.net',
+    url='https://github.com/sababa11/p2.git',
+    install_requires=['boto3==1.14.51', 'p1==0.1'],
     packages=['p2'],
     )
 
