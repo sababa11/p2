d3728d519841209c8785f960c107da74fc4f7a9f
adamt <adamtarasov@gmail.com>
2020-08-30 10:40:08 +0300

    changing structure

--- p2.py
+++ p2.py
@@ -2,4 +2,4 @@ import boto3
 import p1
 
 print("Importing boto3 version:%s" % boto3.__version__)
-print("Importing p1 version:%s" % p1.__version__)
+print("Importing src1 version:%s" % p1.__version__)
