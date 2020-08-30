aad372e1596ec818ee6bb1387963075e7bf1c3d0
adamt <adamtarasov@gmail.com>
2020-08-30 11:22:13 +0300

    modifying package files

--- p2/p2.py
+++ p2/p2.py
@@ -1,5 +1,8 @@
 import boto3
 import p1
 
-print("Importing boto3 version:%s" % boto3.__version__)
-print("Importing src1 version:%s" % p1.__version__)
+
+def print_dependencies():
+    print("Importing boto3 version:%s" % boto3.__version__)
+    print("Importing src1 version:%s" % p1.__version__)
+    return True
